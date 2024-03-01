dabb050ba09a1d334ff70af4f47f16e9f2619650
Md Sayed Arifin <zinnatsaverdhaka@gmail.com>
2024-03-01 11:15:50 +0600

    added frontend Ask a question

--- src/app/(root)/ask-question/page.tsx
+++ src/app/(root)/ask-question/page.tsx
@@ -1,8 +1,14 @@
+import Question from '@/components/form/Question'
+import React from 'react'
 
 const page = () => {
-
     return (
-        <div>aa</div>
+        <div>
+            <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
+            <div className="mt-9">
+                <Question type='Post' />
+            </div>
+        </div>
     )
 }
 
